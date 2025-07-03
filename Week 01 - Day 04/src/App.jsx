ebf7c98f571952c03d8aa727ce633aa4b89ead5b
Fang Lee <phanhung18082003@gmail.com>
2025-07-04 06:25:54 +0700

    Week 01 - Day 04

--- Week 01 - Day 04/src/App.jsx
+++ Week 01 - Day 04/src/App.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AfternoonPractices from "./pages/AfternoonPractices";
+import HomeworkPractices from "./pages/HomeworkPractices";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<AfternoonPractices />} />
+        <Route path="/homework" element={<HomeworkPractices />} />
       </Routes>
     </Router>
   );
