277c93d382c7a2eba07bc66a8d244caa83950693
Fang Lee <phanhung18082003@gmail.com>
2025-07-10 16:49:05 +0700

    Week 02 - Day 09

---
+++ Week 02 - Day 09/src/pages/MyTasks.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import TaskList from '../components/TaskList';
+import Loading from '../components/Loading';
+import { useEffect, useState } from 'react';
+import { getTasksByAssignee } from '../services';
+import type { Task } from '../types';
+
+const MyTasks: React.FC = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [loading, setLoading] = useState(true);
+  // TODO: Replace with actual user id from auth context
+  const userId = 1;
+
+  useEffect(() => {
+    setLoading(true);
+    getTasksByAssignee(userId)
+      .then(setTasks)
+      .finally(() => setLoading(false));
+  }, [userId]);
+
+  if (loading) return <Loading />;
+  return <TaskList tasks={tasks} />;
+};
+
+export default MyTasks;
---
+++ Week 02 - Day 09/src/pages/OurTasks.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import TaskList from '../components/TaskList';
+import Loading from '../components/Loading';
+import { useEffect, useState } from 'react';
+import { getTasks } from '../services';
+import type { Task } from '../types';
+
+const OurTasks: React.FC = () => {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    getTasks()
+      .then(setTasks)
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return <Loading />;
+  return <TaskList tasks={tasks} />;
+};
+
+export default OurTasks;
